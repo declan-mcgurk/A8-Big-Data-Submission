b40a48d356e3af202122264243b233ca7951e0c1
declan-mcgurk <dtm22@students.uwf.edu>
2024-01-26 15:52:20 -0600

    fixed windowing

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from tqdm import tqdm
 
 from datapreprocessing import find_missing
 from featureengineering import fft_df
+from windowing import windowing
 
 def main():
-    fft_df()
+    PATH = "./compdata/dft/0_0-dft.csv"
+    data = pd.read_csv(PATH)
+
+    new = windowing(data, 5)
+    print(new)
+    print(new.shape)
 
 if __name__ == "__main__":
     main()
--- windowing.py
+++ windowing.py
@@ -1,11 +1,15 @@
-def windowing(data: pd.DataFrame, intervalsize: int) -> pd.DataFrame:
+import pandas as pd
+import numpy as np
 
+def windowing(data: pd.DataFrame, intervalsize: int) -> pd.DataFrame:
+    print("windowing...")
     # transform it into numpy array
     array_data = data.values
 
     # Connect the rows and create new array
     array_data_new = np.empty(intervalsize * array_data.shape[1])
-    for i in np.arange(0, array_data.shape[0], intervalsize):
+    for i in np.arange(0, array_data.shape[0] - intervalsize + 1):
+        print(i)
         temp = array_data[i:i + intervalsize].flatten()
         array_data_new = np.vstack([array_data_new, temp])
 
