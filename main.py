82109a6a139ba58f7fe00c589cd0f39e2c0d6810
declan-mcgurk <dtm22@students.uwf.edu>
2024-01-26 13:49:28 -0600

    add data

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    # import the data
+    PATH = "./compdata/bdhsc_2024/stage1_labeled/0_0.csv"
+    data = pd.read_csv(PATH)
+    
+
+
+if __name__ == "__main__":
+    main()
